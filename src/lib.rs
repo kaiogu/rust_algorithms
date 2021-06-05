5e99832e16b0cdafec689676b2027905b16c2a15
Kaio Giurizatto Utsch <kaiogiurizatto@gmail.com>
2021-06-05 21:36:23 +0200

    Make binary search generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-pub fn binary_search(&val: &i32, vec: &Vec<i32>) -> bool {
+pub fn binary_search<T: PartialOrd>(val: &T, vec: &Vec<T>) -> bool {
     let mut low: usize = 0;
     let mut high: usize = vec.len();
     let mut middle: usize;
     while high - low > 0 {
         middle = (high - low)/2 + low;
-        if vec[middle] < val {
+        if vec[middle] < *val {
             low = middle + 1;
-        } else if vec[middle] > val {
+        } else if vec[middle] > *val {
             high = middle;
         } else {
             return true
@@ -50,4 +50,20 @@ mod tests {
     fn binary_search_long() {
         assert_eq!(binary_search(&-300, &vec![-2000, -300, 0, 12, 26, 27, 28, 29, 42 ,43,44]), true);
     }
+
+    #[test]
+    fn binary_search_float() {
+        assert_eq!(binary_search(&-300f64, &vec![-2000.0, -300.0, 0.0, 12.0, 26.0, 27.0, 28.0, 29.0, 42.0, 43.0, 44.0]), true);
+    }
+
+    #[test]
+    fn binary_search_char() {
+        assert_eq!(binary_search(&'O', &vec!['A', 'E', 'I', 'O', 'U']), true);
+    }
+
+    #[test]
+    fn binary_search_char_fail() {
+        assert_eq!(binary_search(&'7', &vec!['A', 'E', 'I', 'O', 'U']), false);
+    }
+
 }
